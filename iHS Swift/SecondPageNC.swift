b68666a5549e02ab0f1073aa7f048badd1c0e5d0
arash <arash@arashs-macbook-pro.local>
2016-11-09 17:35:46 +0330

    Arash - Topbar Created (Not Complete Yet)

---
+++ iHS Swift/SecondPageNC.swift
@@ -0,0 +1,36 @@
+//
+//  SecondPageNC.swift
+//  iHS Swift
+//
+//  Created by arash on 11/9/16.
+//  Copyright © 2016 Ali Zare Sh. All rights reserved.
+//
+
+import UIKit
+
+class SecondPageNC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "asdada"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iHS Swift/TopBar.swift
@@ -0,0 +1,39 @@
+//
+//  TopBar.swift
+//  iHS Swift
+//
+//  Created by arash on 11/9/16.
+//  Copyright © 2016 Ali Zare Sh. All rights reserved.
+//
+
+import UIKit
+
+class TopBar: UIView {
+    var view : UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        createNib()
+    }
+    
+    
+    
+    func createNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "TopBar", bundle: bundle)
+        
+        
+        view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        view.frame = bounds
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth , UIViewAutoresizing.FlexibleHeight]
+        
+        addSubview(view)
+    }
+    
+
+}
