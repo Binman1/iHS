cd6d3a5118f2bb82a30433c33495aadc67311f9f
Ali Zare Sh <alizsh22@gmail.com>
2016-11-12 09:56:46 +0330

    BinMan1 : Commit Edit DB Connection

--- iHS Swift/DBManager.swift
+++ iHS Swift/DBManager.swift
@@ -13,9 +13,10 @@ import Foundation
     BinMan1 : Class For managing the DataBase
  */
 class DBManager {
-    /// Get Some Translation of Sentense 
+    /// BinMan1 : Get Some Translation of Sentense 
     class func getTranslationOfSentences(SentencesID ids : [Int]) -> Array<String> {
         let db = GetDBFromPath()
+        db.open()
         do {
             var resultStringArray = Array<String>()
             for id in ids {
@@ -35,31 +36,36 @@ class DBManager {
     }
     
     
-    /// Get Language ID From DB
+    /// BinMan1 : Get Language ID From DB
     class func getLanguageID () -> Int {
         let db = GetDBFromPath()
+        db.open()
         do {
             var LangID : Int = Int()
             let result = try db.executeQuery("SELECT * FROM Setting WHERE type = LanguageID", values: nil)
             if result.next() {
                 LangID = Int(result.intForColumn("value"))
             }
+            db.close()
             return LangID
         } catch let error as NSError {
             Printer("DBManger Get Language ID Error : \(error.debugDescription)")
+            db.close()
             return -1
         }
     }
     
     
-    /// Set Language ID To DB
+    /// BinMan1 : Set Language ID To DB
     class func setLanguageID (SelectedLanguageID id : Int) {
         let db = GetDBFromPath()
+        db.open()
         do {
             try db.executeUpdate("UPDATE Settings SET value = %d WHERE type = LanguageID", values: [id])
         } catch let error as NSError {
             Printer("DBManager Set Language ID Error : \(error.debugDescription)")
         }
+        db.close()
     }
     
 }
